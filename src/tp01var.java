3adb1c2c731fb2e3a68db66330d76417256459f8
mbenjelloun <>
2026-09-22 16:39:50 +0200

    question 2

---
+++ src/tp01var.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author mbenjelloun
+ */
+public class tp01var {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Ce programme permet\n d'ecrire sur plusieurs lignes");
+    }
+    
+}
