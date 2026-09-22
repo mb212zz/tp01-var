8ebf47cc3575614110936b24e7437dd1a9985b36
mbenjelloun <>
2026-09-22 17:14:21 +0200

    question 4

--- src/tp01var.java
+++ src/tp01var.java
@@ -7,13 +7,29 @@
  *
  * @author mbenjelloun
  */
+import java.util.Scanner
 public class tp01var {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Ce programme permet\n d'ecrire sur plusieurs lignes");
+        //(QUESTION 2) System.out.println("Ce programme permet\n d'ecrire sur plusieurs lignes");
+        
+        
+        Scanner sc = new Scanner(System.in);
+        int x = 0;
+        int y = 0;
+        int temp = 0;
+        System.out.print("Saisir x :");
+        x = sc.nextInt();
+        System.out.print("Saisir y :");
+        y = sc.nextInt();
+        System.out.print("Avant permutation x=" + x + " y=" + y);
+        temp = x;
+        x = y;
+        y = temp;
+        System.out.print("Après permutation x=" + x + " y=" + y);
     }
     
 }
